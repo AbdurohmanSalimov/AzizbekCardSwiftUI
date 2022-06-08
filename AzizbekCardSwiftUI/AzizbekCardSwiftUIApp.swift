ca9b7c88d38c6f07bc5b1e2b3116f082e65ff4d1
AzizbekSalimov <azizbek.salimov.1995@mail.ru>
2022-06-08 16:34:07 +0500

    Initial Commit

---
+++ AzizbekCardSwiftUI/AzizbekCardSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzizbekCardSwiftUIApp.swift
+//  AzizbekCardSwiftUI
+//
+//  Created by Azizbek Salimov on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AzizbekCardSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
